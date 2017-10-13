6739d8c4f5ed7205fdebd1f3dd571b0ed79d107e
Divicoin <info@diviproject.org>
2017-10-12 20:26:35 -0700

    it's not done

--- public/functions.js
+++ public/functions.js
@@ -1,31 +1,54 @@
 // Load articles and display them on page
-$('document').ready(function() {
+$(document).ready(function() {
     $.ajax({
         method: 'GET',
         url: '/articles'
     }).done(function(data) {
         for (var i = 0; i < data.length; i++) {
-            console.log(data[i].title);
-            console.log(data[i].link);
+            // console.log(data[i].title);
+            // console.log(data[i].link);
             var adiv = $('.articles');
             adiv.prepend('<div class="card hoverable">' + 
             '<div class="card-content">' + '<span class="card-title">' + data[i].title + '</span>' + 
-            '<div class="card-action"><a href="' + data[i].link + '"> Read Article </a><a href="#modal1" class="modal-trigger" id="note"> Make Note </a>' 
+            '<div class="card-action"><a href="' + data[i].link + '"> Read Article </a>' + 
+            '<a href="#modal1" class="modal-trigger" data-id="' + data[i]._id + '" ' + 'id="note"> Make Note </a>' + 
+            '<a href="#" data-id="' + data[i]._id + '">See Notes</a>' +
+            '<a href="#">Save Article</a>' 
             + '</div></div></div>')
         }
         
     });
-});
 
-$(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
     $('#note').on('click', function() {
         $('#modal1').modal('open');
+        var thisId = $('#note').attr('data-id');
+        $('#savenote').attr('data-id', thisId);
+        console.log(thisId);
+        $.ajax({
+          method: 'GET',
+          url: '/aritcles/' + thisId  
+        }).done(function(data) {
+            console.log(data);
+            
+        });
+    });
+    $('#savenote').on('click', function() {
+        var thisId = $(this).attr('data-id');
+        $.ajax({
+            method: 'POST',
+            url: '/articles/' + thisId,
+            data: {
+                title: $('#titleInput').val(),
+                body: $('#bodyInput').val()
+            }
+        }).done(function(data) {
+            console.log(data);
+        });
+        $('#titleInput').val('');
+        $('#bodyInput').val('');
     })
 });
-
-
 
 $('#scrape').on('click', function(e) {
     e.preventDefault();
